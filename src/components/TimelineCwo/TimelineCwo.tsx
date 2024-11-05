6c259d52935b651c09794e1e7066f37f431cf3af
MeiWR <malchertomm@gmail.com>
2024-11-05 12:18:38 +0100

    s

--- src/components/TimelineCwo/TimelineCwo.tsx
+++ src/components/TimelineCwo/TimelineCwo.tsx
@@ -11,7 +11,7 @@ function TimelineCwo() {
                 <TimelineCwoItem leftItem={'01.11.2024 - 10.11.2024'} rightItem={'1. kolo registrace: Jednotlivec 699 Kč, Tým 2 024 Kč'} />
                 <TimelineCwoItem leftItem={'11.11.2024 - 24.11.2024'} rightItem={'2. kolo registrace: Jednotlivec 899 Kč, Tým 2 424 Kč'} />
                 <TimelineCwoItem leftItem={'25.11.2024 - 08.12.2024'} rightItem={'3. kolo registrace: Jednotlivec 1 099 Kč, Tým 2 824 Kč'} />
-                <TimelineCwoItem leftItem={'09.12.2024 - 15.12.2024'} rightItem={'3. kolo registrace: Jednotlivec 1 299 Kč, Tým 3 224 Kč'} />
+                <TimelineCwoItem leftItem={'09.12.2024 - 15.12.2024'} rightItem={'4. kolo registrace: Jednotlivec 1 299 Kč, Tým 3 224 Kč'} />
                 <TimelineCwoItem leftItem={'21.12.2024 - 22.12.2024'} rightItem={'Termín konání soutěže'} isLast/>
             </Timeline>
             </div>
