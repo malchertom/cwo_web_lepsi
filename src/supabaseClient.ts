451438fa7402e7d4f8c118a3eae6aff9f4fbdb52
Tomáš Malcher <malchertomm@gmail.com>
2025-09-30 10:15:07 +0200

    redeploy

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY as string;
 
-
+ 
 console.log("Supabase URL:", supabaseUrl);
 console.log("Supabase Key:", supabaseKey); // Make sure this prints a long string
 
